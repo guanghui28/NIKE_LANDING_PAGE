6a39729380dca030dd17b5aa00971b3027419d87
tylerdurden2k2 <phamquanghuy2809@gmail.com>
2024-06-01 19:26:57 +0700

    create the structure for the app

---
+++ src/sections/index.js
@@ -0,0 +1,19 @@
+import Hero from "./Hero";
+import PopularProducts from "./PopularProducts";
+import Subscribe from "./Subscribe";
+import SuperQuality from "./SuperQuality";
+import Footer from "./Footer";
+import CustomerReviews from "./CustomerReviews";
+import SpecialOffers from "./SpecialOffers";
+import Services from "./Services";
+
+export {
+	Hero,
+	PopularProducts,
+	Subscribe,
+	SuperQuality,
+	Footer,
+	CustomerReviews,
+	SpecialOffers,
+	Services,
+};
